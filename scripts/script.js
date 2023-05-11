8e1a93185893b79bf69886e519aec75e19b30bb6
4lfa04 <90815139+4lfa04@users.noreply.github.com>
2023-05-10 21:08:13 -0400

    Add files via upload

---
+++ scripts/script.js
@@ -0,0 +1,12 @@
+let textoFlotante = document.querySelector('.texto-flotante');
+let formulario = document.querySelector('form')
+
+formulario.addEventListener("mouseenter",()=>{
+    // textoFlotante.setAttribute('style','display:block; position: fixed;')
+    // setTimeout(()=>{
+    //     textoFlotante.setAttribute('style','display:none')
+    // },7000)
+})
+// formulario.addEventListener("mouseout",()=>{
+//     textoFlotante.setAttribute('style','display:none')
+// })
